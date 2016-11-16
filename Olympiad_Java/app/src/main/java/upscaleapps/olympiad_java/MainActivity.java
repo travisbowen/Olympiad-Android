21a4618e109ac01ad89aed7b82a7246515f60796
travisbowen <travisbowen@fullsail.edu>
2016-11-15 16:00:21 -0800

    Firebase and login implemented
    Sign in with Firebase is completed

--- Olympiad_Java/app/src/main/java/upscaleapps/olympiad_java/MainActivity.java
+++ Olympiad_Java/app/src/main/java/upscaleapps/olympiad_java/MainActivity.java
@@ -1,32 +1,61 @@
 package upscaleapps.olympiad_java;
 
+
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private EditText userEmailET;
+    private EditText userPassET;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        mAuth = FirebaseAuth.getInstance();
+
+        userEmailET = (EditText)findViewById(R.id.userEmailET);
+        userPassET = (EditText)findViewById(R.id.userPassET);
+        Button loginButton = (Button)findViewById(R.id.loginButton);
+        Button signUpButton = (Button)findViewById(R.id.signUpButton);
+
+        loginButton.setOnClickListener(this);
+        signUpButton.setOnClickListener(this);
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d("Success", "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d("Error", "onAuthStateChanged:signed_out");
+                }
             }
-        });
+        };
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -34,6 +63,7 @@ public class MainActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -48,5 +78,68 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    public void onClick(View view) {
+
+        switch (view.getId()) {
+
+            case R.id.loginButton:
+                login();
+                break;
+
+            case R.id.signUpButton:
+                signUp();
+                break;
+        }
+    }
+
+
+    //Attach the listener to FirebaseAuth instance
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+
+    //Remove the listener to FirebaseAuth instance
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+
+    //Login to Firebase
+    public void login(){
+        if(userEmailET.getText().toString() != "" && userPassET.getText().toString() != "" &&
+                userEmailET.getText().toString().contains("@") && userEmailET.getText().toString().contains(".com")){
+
+            mAuth.signInWithEmailAndPassword(userEmailET.getText().toString(), userPassET.getText().toString())
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            Log.d("SignIn Success", "signInWithEmail:onComplete:" + task.isSuccessful());
+
+                            // If sign in fails, display a message to the user. If sign in succeeds
+                            // the auth state listener will be notified and logic to handle the
+                            // signed in user can be handled in the listener.
+                            if (!task.isSuccessful()) {
+                                Log.w("SignIn Error", "signInWithEmail:failed", task.getException());
+                            }
+                        }
+                    });
+        }
+    }
+
+
+    //Sign Up to Firebase
+    public void signUp(){
+
     }
 }
