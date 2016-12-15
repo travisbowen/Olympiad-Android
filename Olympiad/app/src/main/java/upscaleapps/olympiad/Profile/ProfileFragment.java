a6c965eb53ef185cbc07fc189dd6fc13c0fdeab4
keisto <keisertony@gmail.com>
2016-12-14 21:47:25 -0700

    travis update

--- Olympiad/app/src/main/java/upscaleapps/olympiad/Profile/ProfileFragment.java
+++ Olympiad/app/src/main/java/upscaleapps/olympiad/Profile/ProfileFragment.java
@@ -23,6 +23,8 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 import org.json.JSONException;
 import org.json.JSONObject;
